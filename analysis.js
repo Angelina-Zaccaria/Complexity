f046cc292b331ee742c35ad65f5677e70c71da77
Chris Parnin <chris.parnin@gmail.com>
2015-09-17 20:18:28 -0400

    Adding some more helper functions to default set

--- analysis.js
+++ analysis.js
@@ -63,6 +63,7 @@ function traverse(object, visitor)
     var key, child;
 
     visitor.call(null, object);
+
     for (key in object) {
         if (object.hasOwnProperty(key)) {
             child = object[key];
@@ -72,6 +73,27 @@ function traverse(object, visitor)
         }
     }
 }
+
+// A function following the Visitor pattern.
+// Annotates nodes with parent objects.
+function traverseWithParents(object, visitor)
+{
+    var key, child;
+
+    visitor.call(null, object);
+
+    for (key in object) {
+        if (object.hasOwnProperty(key)) {
+            child = object[key];
+            if (typeof child === 'object' && child !== null && key != 'parent') 
+            {
+            	child.parent = object;
+					traverseWithParents(child, visitor);
+            }
+        }
+    }
+}
+
 
 // A function following the Visitor pattern but allows canceling transversal if visitor returns false.
 function traverseWithCancel(object, visitor)
@@ -98,7 +120,7 @@ function complexity(filePath)
 
 	var i = 0;
 	// Tranverse program with a function visitor.
-	traverse(ast, function (node) 
+	traverseWithParents(ast, function (node) 
 	{
 		if (node.type === 'FunctionDeclaration') 
 		{
@@ -109,8 +131,50 @@ function complexity(filePath)
 
 			builders[builder.FunctionName] = builder;
 		}
+
 	});
 
+}
+
+// Helper function for counting children of node.
+function childrenLength(node)
+{
+	var key, child;
+	var count = 0;
+	for (key in node) 
+	{
+		if (node.hasOwnProperty(key)) 
+		{
+			child = node[key];
+			if (typeof child === 'object' && child !== null && key != 'parent') 
+			{
+				count++;
+			}
+		}
+	}	
+	return count;
+}
+
+
+// Helper function for checking if a node is a "decision type node"
+function isDecision(node)
+{
+	if( node.type == 'IfStatement' )
+	{
+		// Don't double count else/else if
+		if( node.parent && node.parent.type == 'IfStatement' && node.parent["alternate"] )
+		{
+			return false;
+		}
+		return true;
+	}
+
+	if( node.type == 'ForStatement' || node.type == 'WhileStatement' ||
+		 node.type == 'ForInStatement' || node.type == 'DoWhileStatement')
+	{
+		return true;
+	}
+	return false;
 }
 
 // Helper function for printing out function name.
@@ -207,7 +271,7 @@ remainder.toString() + " seconds";
 				      && n != 'A' && n != 'B' && n != 'C' && n != 'D'
 				      && n != 'E' && n != 'F' && n != 'G' && n != 'H'
 				      && n != 'I' && n != 'J' && n != 'K' && n != 'L'
-				      && n != 'M' && n != 'N' && n != 'O' && n != 'P'
+				      && n != 'M' && n != 'N' &&  n != 'O' && n != 'P'
 				      && n != 'Q' && n != 'R' && n != 'S' && n != 'T'
 				      && n != 'U' && n != 'V' && n != 'W' && n != 'X'
 				      && n != 'Y' && n != 'Z'
@@ -229,5 +293,3 @@ mints.toString().split(".")[0] + " " + szmin;
       }
   }
  
-
-

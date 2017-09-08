50aa2384c78187c65a02204eb58d3145971c644d
htan5 <htan5@ncsu.edu>
2017-09-07 22:39:09 -0400

    change test file

--- test/main.js
+++ test/main.js
@@ -1,17 +1,20 @@
-var expect    = require("chai").expect;
+var chai    = require("chai");
+var assert = chai.assert,
+    expect = chai.expect;
 var main = require("../analysis.js");
 
 describe("Sanity check", function() {
   describe("runs", function() {
     it("doesn't crash", function() {
-       // main.FunctionBuilder("analysis.js")
+       main.complexity("analysis.js")
     });
   });
 
 
-// //   // describe("fuzzer works", function() {
-// //   //   it("mutate string", function() {
-// //   //       // main.fuzzer.mutate.string("hello");
-// //   //   });
-// //   // });
+  // describe("fuzzer works", function() {
+  //   it("mutate string", function() {
+  //       expect(main.complexity("analysis.js")).to.not.throw();
+  //       assert.()
+  //   });
+  // });
 });
